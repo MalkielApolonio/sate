dba2dbe6f891589dc65310e0fea3ba03c70b72e7
MalkielApolonio <malkielivanapolonio@gmail.com>
2018-02-27 16:48:59 -0800

    may add php

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bakery Sate: Prototype</title>
+    <link rel="css" href="/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="page-wrapper">
+
+<!--====    Header   ====--->
+
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+          <div class="container">
+            <a class="navbar-brand">
+              <img src="" alt="">
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i class="fa fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Order</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Menu</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>  <!-- close container -->
+        </nav>
+      </header>
+
+<!-- =====  Hero Image  ===== -->
+
+      <div class="hero-img text-center">
+        <h2>YUMMINESS FROM SCRATCH<h2>​
+        <h3>One of the Top 20 Bakeries in Canada - Huffington Post<h3>
+        <h3>As seen on Food Network Sugar Showdown Season 2</h3>
+      </div> <!--- close hero-img -->
+
+<!--  ====  Services  ==== -->
+      <div class="services">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="icon">
+                <img src="" alt="">
+              </div>
+              <div class="text-center">
+                <h3> Menu </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="icon">
+                <img src="" alt="">
+              </div>
+              <div class="text-center">
+                <h3> Order </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </div>
+            </div>
+          </div>
+        </div> <!--- colose container -->
+      </div> <!--- close services --->
+
+<!---  ===== Footer =====  ---->
+
+      <footer class="container footer">
+        <div class="row">
+          <div class="col-lg-6">
+            <p>(604) 506 0290</p>
+            <p>2879 Commercial Drive, Vancouver, BC</p>
+            <p>feedback@bakerysate.com</p>
+          </div>
+          <div class="col-lg-6">
+            <img src="" alt="">
+          </div>
+        </div>
+      </footer>
+
+
+</div> <!--- close page-wrapper --->
+  </body>
+</html>
